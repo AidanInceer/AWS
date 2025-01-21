18b71bd8342cda01a432c9d4e707d5f1e1864761
AidanInceer <aidaninceer0@gmail.com>
2025-01-21 14:43:18 +0000

    add basic setup for aws bucket

--- main.tf
+++ main.tf
@@ -9,4 +9,14 @@ terraform {
 
 provider "aws" {
   region = "eu-west-2"
+  profile = "default"
+}
+
+resource "aws_s3_bucket" "terra-test-bucket-aws" {
+  bucket = "terra-test-bucket-aws"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
