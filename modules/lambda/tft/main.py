44525c0cdc9a0912a634732d82eab6b698af554f
AidanInceer <aidaninceer0@gmail.com>
2025-01-24 13:14:52 +0000

    fix secret fetching

--- modules/lambda/tft/main.py
+++ modules/lambda/tft/main.py
@@ -14,24 +14,23 @@ def lambda_handler(event, context):
 def load_user_data():
     print("fetching secret")
     secret = get_secret()
-    divisions = ["master", "grandmaster", "challenger"]
-    print("fetching data")
-    for division in divisions:
-        url = f"https://euw1.api.riotgames.com/tft/league/v1/{division}?queue=RANKED_TFT&api_key={secret}"
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
-            "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
-            "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-            "Origin": "https://developer.riotgames.com",
-        }
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            print(response.status_code)
-            return response.json()
-        else:
-            print(f"Failed to get summoner: {response.status_code}, {response.text}")
-            print(response.text)
-            return None
+
+    url = "https://euw1.api.riotgames.com/tft/league/v1/challenger?queue=RANKED_TFT"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+        "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+        "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Origin": "https://developer.riotgames.com",
+        "X-Riot-Token": f"{secret}",
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        print(response.status_code)
+        return response.json()
+    else:
+        print(f"Failed to get summoner: {response.status_code}, {response.text}")
+        print(response.text)
+        return None
 
 
 def get_secret():
@@ -42,10 +41,18 @@ def get_secret():
     client = session.client(service_name="secretsmanager", region_name=region_name)
 
     try:
-        return client.get_secret_value(SecretId=secret_name)["SecretString"]
+        return (
+            client.get_secret_value(SecretId=secret_name)["SecretString"]
+            .split(":")[1]
+            .replace('"', "")
+            .replace("}", "")
+        )
     except ClientError as e:
         raise e
 
+
+if __name__ == "__main__":
+    load_user_data()
 
 """
 1. /tft/league/v1/challenger access league/account ids
